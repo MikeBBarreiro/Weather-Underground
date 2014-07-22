e9c5155d658d26368ac6ee01a37be2f8f4fbc745
MikeBBarreiro <MikeBBarreiro@gmail.com>
2014-07-22 16:42:02 -0500

    included avghigh and lows

--- app/models/weather.js
+++ app/models/weather.js
@@ -52,10 +52,53 @@ Weather.avgLow = function(zip, cb){
     cb(avgl);
   });
 };
-    
 
+Weather.allHighs = function(zip, cb){
+  var url = 'http://api.wunderground.com/api/d8b5a108b79cf7eb/forecast10day/q/CA/' + zip + '.json';
+  request(url, function(error, response, body){
+    body= JSON.parse(body);
+    var aHigh = [];
 
+    for(var i = 0; i < body.forecast.simpleforecast.forecastday.length; i++){
+      parseInt(body.forecast.simpleforecast.forecastday[i].high.fahrenheit);
+    }
+    aHigh.push(body.forecast.simpleforecast.forecastday.high);
 
+    cb(aHigh);
+
+  });
+};
+Weather.allLows = function(zip, cb){
+  var url = 'http://api.wunderground.com/api/d8b5a108b79cf7eb/forecast10day/q/CA/' + zip + '.json';
+  request(url, function(error, response, body){
+    body= JSON.parse(body);
+    var aLow = [];
+
+    for(var i = 0; i < body.forecast.simpleforecast.forecastday.length; i++){
+      parseInt(body.forecast.simpleforecast.forecastday[i].low.fahrenheit);
+     }
+    aLow.push( body.forecast.simpleforecast.forecastday.low);
+    cb(aLow);
+
+  });
+};
+
+Weather.deltas = function(zip, cb){
+  var url = 'http://api.wunderground.com/api/d8b5a108b79cf7eb/forecast10day/q/CA/' + zip + '.json';
+  request(url, function(error, response, body){
+    body= JSON.parse(body);
+  var aDeltas = [];
+  var forecats = body.forecast.simpleforecast.forecastday;
+  
+  for(var i = 0; i <forecats.length; i++){
+  aDeltas.push(parseInt(forecats[i].high.fahrenheit) - parseInt(forecats[i].low.fahrenheit));
+
+  }
+
+ cb(aDeltas);
+
+});
+};
 
 
 module.exports = Weather;
--- test/unit/weather.js
+++ test/unit/weather.js
@@ -16,33 +16,62 @@ describe('Weather', function(){
       });
     });
   });
-});
 
-describe('.low', function(){
-  it('Should return the low temp for today', function(done){
-    Weather.low(10011, function(temp){
-      expect(temp).to.be.ok;
-      expect(temp.length).to.be.at.least(2);
-      done();
+
+  describe('.low', function(){
+    it('Should return the low temp for today', function(done){
+      Weather.low(10011, function(temp){
+        expect(temp).to.be.ok;
+        expect(temp.length).to.be.at.least(2);
+        done();
+      });
+    });
+  });
+
+  describe('.avgHigh', function(){
+    it('Should return the average high', function(done){
+      Weather.avgHigh(70420, function(temp){
+        expect(temp).to.be.within(-50, 150);
+        done();
+      });
+    });
+  });
+
+  describe('.avgLow', function(){
+    it('Should return the average low', function(done){
+      Weather.avgLow(70420, function(temp){
+        expect(temp).to.be.within(-50, 150);
+        done();
+      });
+    });
+  });
+
+  describe('.allHighs', function(){
+    it('Should show all the highs within 10 days', function(done){
+      Weather.allHighs(70420, function(temp){
+        expect(temp).to.be.within(-50,150);
+        done();
+      });
+    });
+  });
+  describe('.allLows', function(){
+    it('Should shows all the lows within 10 days', function(done){
+      Weather.allLows(70420, function(temp){
+        expect(temp).to.be.within(-50, 150);
+        done();
+      });
+    });
+  });
+  describe('.deltas', function(){
+    it('Should give the deference between the, Highs, and Lows', function(done){
+      Weather.deltas(70420, function(aDeltas){
+        expect(aDeltas).to.have.length(10);
+        expect(aDeltas[0]).to.be.within(-50, 150);
+        done();
+      });
     });
   });
 });
 
-describe('.avgHigh', function(){
-  it('Should return the average high', function(done){
-    Weather.avgHigh(70420, function(temp){
-      expect(temp).to.be.within(-50, 150);
-      done();
-    });
-  });
-});
 
-describe('.avgLow', function(){
-  it('Should return the average low', function(done){
-    Weather.avgLow(70420, function(temp){
-      expect(temp).to.be.within(-50, 150);
-      done();
-    });
-  });
-});
 
